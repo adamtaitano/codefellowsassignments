abcfea3552bce90b298c0784daf69157cbb89437
Adam Taitano <adam.taitano@gmail.com>
2015-10-26 15:03:42 -0700

    modify code to verify node command line response

--- Week1/October_26/greet.js
+++ Week1/October_26/greet.js
@@ -3,10 +3,17 @@ var greet = function(name){
   return 'Hello, ' + name; // use return instead of console.log for tests
 };
 
-
+console.log(process.argv);
 //This is an attempt at the first bonus point:
+if(process.argv.length === 3) {
 var param = process.argv[2];
 console.log(greet(param));
+}
+if(process.argv.length === 5) {
+var param = process.argv[4];
+console.log(greet(param));
+}
+
 
 module.exports.greet = greet;
-module.exports.greet.param = param;
+module.exports.param = param;
